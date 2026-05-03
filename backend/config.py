98b9a630d412532a6091429bfb1bfcc57f32efb1
Ayodhya Mahesh Babu <142784557+ayodhya16@users.noreply.github.com>
2026-05-03 23:55:16 +0530

    Update database configuration placeholders

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,9 @@ import os
 
 class config:
     DB_HOST = "localhost"
-    DB_USER = "root"
-    DB_PASSWORD="Babu@mb16"
-    DB_NAME= "taskflow_db"
+    DB_USER = "YOUR_USERNAME"
+    DB_PASSWORD="YOURPASSWORD"
+    DB_NAME= "YOUR DB NAME"
 
-    SECRET_KEY = "a3f2e594eae784f5c9c33db9c26e4d6af71d25ff841b661d36fca7136c044f52"
+    SECRET_KEY = "YOUR SECRET KEY"
     
