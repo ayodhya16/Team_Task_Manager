53d08f02c4f4187c0f9286dfa76aa1c7df257e38
ayodhya16 <maheshbabumb08@gmail.com>
2026-05-06 12:07:44 +0530

    update production backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://team-task-manager-ayodhya16/api",
+    baseURL: "https://team-task-manager-production-502a.up.railway.app/api",
 });
 
 api.interceptors.request.use((config)=>{
